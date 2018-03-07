41dfcbe4635bd2b89346f4f9e2edce6a3b23739f
Victor Kim <hk1798@nyu.edu>
2018-03-07 15:15:00 -0500

    main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string ciphertext, plaintext;
+
+    //Get input: (ciphertext)
+    cout << "Enter the ciphertext: ";
+    cin >> ciphertext;
+
+    //Decrypt
+
+    //Print output: (plaintext)
+    cout << "Plaintext: " << plaintext << endl;
+}
