9057c7c1efe457ca9f4d3032098caf803612bad7
Victor Kim <hk1798@nyu.edu>
2018-03-09 04:22:31 -0500

    Implemented decrypt by using value array

--- decrypt.cpp
+++ decrypt.cpp
@@ -1,2 +1,21 @@
 #include "decrypt.h"
 using namespace std;
+
+void decryptString(string& plaintext, string& ciphertext, char* valArr){
+    //Read ciphertext
+    istringstream cipherStream(ciphertext);
+    string encryptedChar;
+    char del = ',';
+    while(getline(cipherStream, encryptedChar, del)){
+        //cout << encryptedChar << ',';
+        int charNum = stoi(encryptedChar);
+        if(charNum >= 0 && charNum <= 105){ //Valid key
+            //Lookup key in value array
+            plaintext += valArr[charNum];
+        }
+        else{
+            cerr << "Invalid key" << endl;
+            exit(-1);
+        }
+    }
+}
--- decrypt.h
+++ decrypt.h
@@ -3,6 +3,10 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <algorithm>
 
+//Decrypts the ciphertext and stores it into the plaintext
+void decryptString(std::string& plaintext, std::string& ciphertext, char* valArr);
 
 #endif  /* decrypt_h */
