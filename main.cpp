e15ed960d538657e8550d3555ca993d750758b81
Victor Kim <hk1798@nyu.edu>
2018-03-10 22:58:46 -0500

    must add parameter

--- main.cpp
+++ main.cpp
@@ -28,115 +28,103 @@ int main(int argc, char* argv[]){
     //printKeyMap(&keyMap);
     //printValMap(valueMap);
 
-    //Get input: (ciphertext)
-    cout << "Enter the ciphertext: ";
-    cin >> ciphertext;
+    //argv[1] = Chooses which test (either 1 or 2)
+    if(argc == 2){
+        //Get input: (ciphertext)
+        cout << "Enter the ciphertext: ";
+        cin >> ciphertext;
 
-    if(argc <= 1){  //No input arguements
-        //Decrypt
-        bool result = decryptLoop(plaintext, ciphertext, valueMap);
-        if(result){
-            //Print output: (plaintext)
-            cout << "Plaintext: " << plaintext << endl;
-        }
-        else{
-            cout << "Could not decrypt" << endl;
-        }
-    }
-    else{   //Input arguements
-        //argv[1] = Chooses which test (either 1 or 2)
-        if(argc == 2){
-            //Input arguement has to be a number
-            int test = stoi(argv[1]);
-            //Perform test 1
-            if(test == 1){
-                /**
-                //Testing Code:
-                int choice;
-                cout << "Choose plaintext candidate (1~5): ";
-                cin >> choice;
+        //Input arguement has to be a number
+        int test = stoi(argv[1]);
+        //Perform test 1
+        if(test == 1){
+            /**
+            //Testing Code:
+            int choice;
+            cout << "Choose plaintext candidate (1~5): ";
+            cin >> choice;
 
-                switch(choice){
-                    case 1:
-                        encryptString(ciphertext, candidate1, &keyMap);
-                        cout << "Plaintext: " << endl << candidate1 << endl;
-                        break;
-                    case 2:
-                        encryptString(ciphertext, candidate2, &keyMap);
-                        cout << "Plaintext: " << endl << candidate2 << endl;
+            switch(choice){
+                case 1:
+                    encryptString(ciphertext, candidate1, &keyMap);
+                    cout << "Plaintext: " << endl << candidate1 << endl;
+                    break;
+                case 2:
+                    encryptString(ciphertext, candidate2, &keyMap);
+                    cout << "Plaintext: " << endl << candidate2 << endl;
 
-                        break;
-                    case 3:
-                        encryptString(ciphertext, candidate3, &keyMap);
-                        cout << "Plaintext: " << endl << candidate3 << endl;
+                    break;
+                case 3:
+                    encryptString(ciphertext, candidate3, &keyMap);
+                    cout << "Plaintext: " << endl << candidate3 << endl;
 
-                        break;
-                    case 4:
-                        encryptString(ciphertext, candidate4, &keyMap);
-                        cout << "Plaintext: " << endl << candidate4 << endl;
+                    break;
+                case 4:
+                    encryptString(ciphertext, candidate4, &keyMap);
+                    cout << "Plaintext: " << endl << candidate4 << endl;
 
-                        break;
-                    case 5:
-                        encryptString(ciphertext, candidate5, &keyMap);
-                        cout << "Plaintext: " << endl << candidate5 << endl;
+                    break;
+                case 5:
+                    encryptString(ciphertext, candidate5, &keyMap);
+                    cout << "Plaintext: " << endl << candidate5 << endl;
 
-                        break;
-                    default:
-                        //Do nothing
-                        cout << "Choose a number between 1~5" <<endl;
-                        break;
-                }
-                */
-
-                vector<revKey> keyS1, keyS2, keyS3, keyS4, keyS5;
-
-                reverseGenKeyspace(keyS1, candidate1, ciphertext);
-                reverseGenKeyspace(keyS2, candidate2, ciphertext);
-                reverseGenKeyspace(keyS3, candidate3, ciphertext);
-                reverseGenKeyspace(keyS4, candidate4, ciphertext);
-                reverseGenKeyspace(keyS5, candidate5, ciphertext);
-
-                if(testKey(keyS1)){
-                    cout << "Plaintext: " << candidate1 << endl;
-                }
-                else if(testKey(keyS2)){
-                    cout << "Plaintext: " << candidate2 << endl;
-                }
-                else if(testKey(keyS3)){
-                    cout << "Plaintext: " << candidate3 << endl;
-                }
-                else if(testKey(keyS4)){
-                    cout << "Plaintext: " << candidate4 << endl;
-                }
-                else if(testKey(keyS5)){
-                    cout << "Plaintext: " << candidate5 << endl;
-                }
-                else{
-                    cout << "Could not decrypt" << endl;
-                }
+                    break;
+                default:
+                    //Do nothing
+                    cout << "Choose a number between 1~5" <<endl;
+                    break;
             }
-            else if(test == 2){ //Perform test 2
-                /**
-                 * string test2 = "rereads predestines equippers cavitation";
-                 * encryptString(ciphertext, test2, &keyMap);
-                 */
+            */
 
-                //Decrypt
-                bool result = decryptLoop(plaintext, ciphertext, valueMap);
-                if(result){
-                    //Print output: (plaintext)
-                    cout << "Plaintext: " << plaintext << endl;
-                }
-                else{
-                    cout << "Could not decrypt" << endl;
-                }
+            vector<revKey> keyS1, keyS2, keyS3, keyS4, keyS5;
+
+            reverseGenKeyspace(keyS1, candidate1, ciphertext);
+            reverseGenKeyspace(keyS2, candidate2, ciphertext);
+            reverseGenKeyspace(keyS3, candidate3, ciphertext);
+            reverseGenKeyspace(keyS4, candidate4, ciphertext);
+            reverseGenKeyspace(keyS5, candidate5, ciphertext);
+
+            if(testKey(keyS1)){
+                cout << "Plaintext: " << candidate1 << endl;
+            }
+            else if(testKey(keyS2)){
+                cout << "Plaintext: " << candidate2 << endl;
+            }
+            else if(testKey(keyS3)){
+                cout << "Plaintext: " << candidate3 << endl;
+            }
+            else if(testKey(keyS4)){
+                cout << "Plaintext: " << candidate4 << endl;
+            }
+            else if(testKey(keyS5)){
+                cout << "Plaintext: " << candidate5 << endl;
             }
             else{
-                cout << "Enter 1 to perform test 1 and 2 to perform test 2." << endl;
+                cout << "Could not decrypt" << endl;
+            }
+        }
+        else if(test == 2){ //Perform test 2
+            /**
+             * string test2 = "rereads predestines equippers cavitation";
+             * encryptString(ciphertext, test2, &keyMap);
+             */
+
+            //Decrypt
+            bool result = decryptLoop(plaintext, ciphertext, valueMap);
+            if(result){
+                //Print output: (plaintext)
+                cout << "Plaintext: " << plaintext << endl;
+            }
+            else{
+                cout << "Could not decrypt" << endl;
             }
         }
         else{
-            cout << "Enter only 1 or 2 as the arguement." << endl;
+            cout << "Enter 1 to perform test 1 and 2 to perform test 2." << endl;
         }
+    }
+    else{
+        cout << "Enter only 1 or 2 as the arguement." << endl;
+        cout << "Ex) ./Kim-Rabbani-Walker-decrypt 1" << endl;
     }
 }
